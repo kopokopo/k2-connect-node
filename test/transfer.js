ad5c3e74ce32931f1e2d9155fdd6f76682cd172f
Nicollet <nicollet.njora05@gmail.com>
2019-02-18 10:19:21 +0300

    Unit Test - createsettlement()

--- test/transfer.js
+++ test/transfer.js
@@ -37,6 +37,22 @@ describe('TransferService', function () {
             return transfer.settleFunds(opts)
                 .should.be.rejected();
         });
+
+        it('#createSettlementAccount() cannot be empty', function () {
+            return transfer.createSettlementAccount(opts)
+                .should.be.rejected();
+        });
+
+        it('#createSettlementAccount() has to have token details', function () {
+
+            opts.accountName = "my_account_name",
+            opts.bankRef = "89076-9ed38155-7d6f-11e3-83c3-5404a6144203-adiu",
+            opts.bankBranchRef = "9ed38155-7d6f-11e3-83c3-5404a6144203",
+            opts.accountNumber = "1234567890"
+             
+            return transfer.settleFunds(opts)
+                .should.be.rejected();
+        });
     
     })
 
@@ -62,6 +78,28 @@ describe('TransferService', function () {
                     done();
                 });
     })
+    it('#createSettlementAccount()', function (done) {
+        var opts = {}
+
+        opts.accountName = "my_account_name",
+        opts.bankRef = "89076-9ed38155-7d6f-11e3-83c3-5404a6144203-adiu",
+        opts.bankBranchRef = "9ed38155-7d6f-11e3-83c3-5404a6144203",
+        opts.accountNumber = "1234567890"
+        opts.token_details = {"token_type":"Bearer",
+                                "expires_in":3600,
+                                "access_token":"hardToGuessKey"};
+
+
+        transfer.createSettlementAccount(opts)
+                .then(function (response){
+                    // response.should.have.property('resourceId');
+                    done();
+                })
+                .catch(function (error) {
+                    console.error(error);
+                    done();
+                });
+    });
 
     it('#settlementStatus()', function(done){
         var opts = {}
