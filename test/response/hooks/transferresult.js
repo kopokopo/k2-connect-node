81ceb7a0ea2754c93d4f9bac80bd422ca056c7fe
NicolletNjora <nicollet.njora05@gmail.com>
2019-03-21 10:07:40 +0300

    Add webhook mock data for test

---
+++ test/response/hooks/transferresult.js
@@ -0,0 +1,13 @@
+module.exports = {
+	'id': 'd76265cd-0951-e511-80da-0aa34a9b2388',
+	'topic': 'transfers',
+	'status': 'Pending',
+	'completed_at': '2018-05-02T00:30:25.580Z',
+	'amount': {
+		'value': '225.00',
+		'currency': 'KES'
+	},
+	'_links': {
+		'self': 'https://api-sandbox.kopokopo.com/transfers/d76265cd-0951-e511-80da-0aa34a9b2388'
+	}
+}
